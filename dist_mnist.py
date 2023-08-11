6bed8731f3487981545ac0f73e91178aac97e24f
AndreFrigo <andrea.boy.frigo@gmail.com>
2023-08-11 16:43:12 +0200

    .

--- dist_mnist.py
+++ dist_mnist.py
@@ -63,7 +63,7 @@ flags.DEFINE_integer("replicas_to_aggregate", None,
                      "num_workers)")
 flags.DEFINE_integer("hidden_units", 100,
                      "Number of units in the hidden layer of the NN")
-flags.DEFINE_integer("train_steps", 2000,
+flags.DEFINE_integer("train_steps", 20000,
                      "Number of (global) training steps to perform")
 flags.DEFINE_integer("batch_size", 1000, "Training batch size")
 flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
@@ -272,7 +272,6 @@ def main(unused_argv):
     print("Training begins @ %f" % time_begin)
 
     local_step = 0
-    print( np.array(image.reshape(28,28,1) for image in mnist.train.images).size)
     while True:
       # Training feed
       batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
@@ -298,6 +297,14 @@ def main(unused_argv):
     val_xent = sess.run(cross_entropy, feed_dict=val_feed)
     print("After %d training step(s), validation cross entropy = %g" %
           (FLAGS.train_steps, val_xent))
+
+
+    # Test feed
+    test_feed = {x: mnist.test.images, y_: mnist.test.labels}
+    test_xent = sess.run(cross_entropy, feed_dict=test_feed)
+    print("After %d training step(s), test cross entropy = %g" %
+          (FLAGS.train_steps, test_xent))
+  
 
 
 if __name__ == "__main__":
