2d6a529b391acc8001feb555b4356c72f4c993ef
AndreFrigo <45069367+AndreFrigo@users.noreply.github.com>
2023-11-27 19:01:29 +0100

    Original version for debug

--- dist_mnist.py
+++ dist_mnist.py
@@ -45,27 +45,33 @@ import time
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-#number of CPU nodes (same as the number of k8s computing nodes)
-nodes = 3
-cpustring = "localhost:2223"
-for i in range(1,nodes):
-  cpustring += ",localhost:"+str(2223+i)
-
 flags = tf.app.flags
 flags.DEFINE_string("data_dir", "/tmp/mnist-data",
                     "Directory for storing mnist data")
+flags.DEFINE_boolean("download_only", False,
+                     "Only perform downloading of data; Do not proceed to "
+                     "session preparation, model definition or training")
 flags.DEFINE_integer("task_index", None,
                      "Worker task index, should be >= 0. task_index=0 is "
                      "the master worker task the performs the variable "
                      "initialization ")
-flags.DEFINE_integer("num_gpus", 0, "Total number of gpus for each machine."
+flags.DEFINE_integer("num_gpus", 1, "Total number of gpus for each machine."
                      "If you don't use GPU, please set it to '0'")
+flags.DEFINE_integer("replicas_to_aggregate", None,
+                     "Number of replicas to aggregate before parameter update"
+                     "is applied (For sync_replicas mode only; default: "
+                     "num_workers)")
 flags.DEFINE_integer("hidden_units", 100,
                      "Number of units in the hidden layer of the NN")
-flags.DEFINE_integer("train_steps", 300,
+flags.DEFINE_integer("train_steps", 20000,
                      "Number of (global) training steps to perform")
 flags.DEFINE_integer("batch_size", 100, "Training batch size")
 flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
+flags.DEFINE_boolean(
+    "sync_replicas", False,
+    "Use the sync_replicas (synchronized replicas) mode, "
+    "wherein the parameter updates from workers are aggregated "
+    "before applied to avoid stale gradients")
 flags.DEFINE_boolean(
     "existing_servers", False, "Whether servers already exists. If True, "
     "will use the worker hosts via their GRPC URLs (one client process "
@@ -73,7 +79,7 @@ flags.DEFINE_boolean(
     "server.")
 flags.DEFINE_string("ps_hosts", "localhost:2222",
                     "Comma-separated list of hostname:port pairs")
-flags.DEFINE_string("worker_hosts", cpustring,
+flags.DEFINE_string("worker_hosts", "localhost:2223,localhost:2224",
                     "Comma-separated list of hostname:port pairs")
 flags.DEFINE_string("job_name", None, "job name: worker or ps")
 
@@ -89,8 +95,6 @@ IMAGE_PIXELS = 28
 #        'task': {'type': 'worker', 'index': 1}})
 
 def main(unused_argv):
-
-  time_start = time.time()
   # Parse environment variable TF_CONFIG to get job_name and task_index
 
   # If not explicitly specified in the constructor and the TF_CONFIG
@@ -104,6 +108,8 @@ def main(unused_argv):
   FLAGS.task_index = task_index
 
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+  if FLAGS.download_only:
+    sys.exit(0)
 
   if FLAGS.job_name is None or FLAGS.job_name == "":
     raise ValueError("Must specify an explicit `job_name`")
@@ -114,8 +120,6 @@ def main(unused_argv):
   print("task index = %d" % FLAGS.task_index)
 
   cluster_config = tf_config.get('cluster', {})
-  print("CLUSTER CONFIG")
-  print(cluster_config)
   ps_hosts = cluster_config.get('ps')
   worker_hosts = cluster_config.get('worker')
 
@@ -128,10 +132,7 @@ def main(unused_argv):
   # Construct the cluster and start the server
   ps_spec = FLAGS.ps_hosts.split(",")
   worker_spec = FLAGS.worker_hosts.split(",")
-  print("PS_SPEC")
-  print(ps_spec)
-  print("WORKER_SPEC")
-  print(worker_spec)
+
   # Get the number of workers.
   num_workers = len(worker_spec)
 
@@ -157,7 +158,6 @@ def main(unused_argv):
   # The device setter will automatically place Variables ops on separate
   # parameter servers (ps). The non-Variable ops will be placed on the workers.
   # The ps use CPU and workers use corresponding GPU
-  print("WORKING DEVICE: "+worker_device)
   with tf.device(
       tf.train.replica_device_setter(
           worker_device=worker_device,
@@ -190,22 +190,53 @@ def main(unused_argv):
 
     y = tf.nn.softmax(tf.nn.xw_plus_b(hid, sm_w, sm_b))
     cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
     opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
 
+    if FLAGS.sync_replicas:
+      if FLAGS.replicas_to_aggregate is None:
+        replicas_to_aggregate = num_workers
+      else:
+        replicas_to_aggregate = FLAGS.replicas_to_aggregate
+
+      opt = tf.train.SyncReplicasOptimizer(
+          opt,
+          replicas_to_aggregate=replicas_to_aggregate,
+          total_num_replicas=num_workers,
+          name="mnist_sync_replicas")
 
     train_step = opt.minimize(cross_entropy, global_step=global_step)
+
+    if FLAGS.sync_replicas:
+      local_init_op = opt.local_step_init_op
+      if is_chief:
+        local_init_op = opt.chief_init_op
+
+      ready_for_local_init_op = opt.ready_for_local_init_op
+
+      # Initial token and chief queue runners required by the sync_replicas mode
+      chief_queue_runner = opt.get_chief_queue_runner()
+      sync_init_op = opt.get_init_tokens_op()
 
     init_op = tf.global_variables_initializer()
     train_dir = tempfile.mkdtemp()
 
-    sv = tf.train.Supervisor(
-        is_chief=is_chief,
-        logdir=train_dir,
-        init_op=init_op,
-        recovery_wait_secs=1,
-        global_step=global_step)
+    if FLAGS.sync_replicas:
+      sv = tf.train.Supervisor(
+          is_chief=is_chief,
+          logdir=train_dir,
+          init_op=init_op,
+          local_init_op=local_init_op,
+          ready_for_local_init_op=ready_for_local_init_op,
+          recovery_wait_secs=1,
+          global_step=global_step)
+    else:
+      sv = tf.train.Supervisor(
+          is_chief=is_chief,
+          logdir=train_dir,
+          init_op=init_op,
+          recovery_wait_secs=1,
+          global_step=global_step)
 
     sess_config = tf.ConfigProto(
         allow_soft_placement=True,
@@ -230,54 +261,42 @@ def main(unused_argv):
       sess = sv.prepare_or_wait_for_session(server.target, config=sess_config)
 
     print("Worker %d: Session initialization complete." % FLAGS.task_index)
-       
-    
+
+    if FLAGS.sync_replicas and is_chief:
+      # Chief worker will start the chief queue runner and call the init op.
+      sess.run(sync_init_op)
+      sv.start_queue_runners(sess, [chief_queue_runner])
+
     # Perform training
     time_begin = time.time()
     print("Training begins @ %f" % time_begin)
-    print("Nodes="+str(nodes))
+
     local_step = 0
-    #control to print data one time every 1000 steps
-    hasPrinted = False
     while True:
       # Training feed
       batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
       train_feed = {x: batch_xs, y_: batch_ys}
 
       _, step = sess.run([train_step, global_step], feed_dict=train_feed)
-      print("Done step "+str(step))
       local_step += 1
+      print("Done step: "+str(step))
       now = time.time()
-      if (step % 1000 <= 200 and not hasPrinted and step >= 1000):
-        print("time: %f, step: %d" % (now, step-(step%1000)))
-        hasPrinted = True
-      if (step % 1000 > 200):
-        hasPrinted = False
+      print("%f: Worker %d: training step %d done (global step: %d)" %
+            (now, FLAGS.task_index, local_step, step))
+
       if step >= FLAGS.train_steps:
         break
 
     time_end = time.time()
     print("Training ends @ %f" % time_end)
-    print("Steps: %d, Batch size: %d" % (FLAGS.train_steps, FLAGS.batch_size))
     training_time = time_end - time_begin
-    starting_time = time_begin - time_start
-    print("Starting time (time lost before starting training): %f s" % starting_time)
     print("Training elapsed time: %f s" % training_time)
-    
-    if is_chief:
-      # Validation feed
-      val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
-      val_acc = sess.run(accuracy, feed_dict=val_feed)
-      print("Validation accuracy = %g" %
-            (val_acc*100))
-  
-  
-      # Test feed
-      test_feed = {x: mnist.test.images, y_: mnist.test.labels}
-      test_acc = sess.run(accuracy, feed_dict=test_feed)
-      print("Test accuracy = %g" %
-            (test_acc*100))
 
+    # Validation feed
+    val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
+    val_xent = sess.run(cross_entropy, feed_dict=val_feed)
+    print("After %d training step(s), validation cross entropy = %g" %
+          (FLAGS.train_steps, val_xent))
 
 
 if __name__ == "__main__":
