b84641fa6443556fea5f0dae0b61bead7732bbb2
AndreFrigo <45069367+AndreFrigo@users.noreply.github.com>
2023-11-28 08:57:47 +0100

    Update dist_mnist.py

--- dist_mnist.py
+++ dist_mnist.py
@@ -62,7 +62,7 @@ flags.DEFINE_integer("num_gpus", 0, "Total number of gpus for each machine."
                      "If you don't use GPU, please set it to '0'")
 flags.DEFINE_integer("hidden_units", 100,
                      "Number of units in the hidden layer of the NN")
-flags.DEFINE_integer("train_steps", 100,
+flags.DEFINE_integer("train_steps", 20000,
                      "Number of (global) training steps to perform")
 flags.DEFINE_integer("batch_size", 100, "Training batch size")
 flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
