50548a309af09cd0ad36f750be3130f20d0786fd
AndreFrigo <45069367+AndreFrigo@users.noreply.github.com>
2023-11-28 08:20:22 +0100

    My version

--- dist_mnist.py
+++ dist_mnist.py
@@ -45,25 +45,24 @@ import time
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+#number of CPU nodes (same as the number of k8s computing nodes)
+nodes = 9
+cpustring = "localhost:2223"
+for i in range(1,nodes):
+  cpustring += ",localhost:"+str(2223+i)
+
 flags = tf.app.flags
 flags.DEFINE_string("data_dir", "/tmp/mnist-data",
                     "Directory for storing mnist data")
-flags.DEFINE_boolean("download_only", False,
-                     "Only perform downloading of data; Do not proceed to "
-                     "session preparation, model definition or training")
 flags.DEFINE_integer("task_index", None,
                      "Worker task index, should be >= 0. task_index=0 is "
                      "the master worker task the performs the variable "
                      "initialization ")
-flags.DEFINE_integer("num_gpus", 1, "Total number of gpus for each machine."
+flags.DEFINE_integer("num_gpus", 0, "Total number of gpus for each machine."
                      "If you don't use GPU, please set it to '0'")
-flags.DEFINE_integer("replicas_to_aggregate", None,
-                     "Number of replicas to aggregate before parameter update"
-                     "is applied (For sync_replicas mode only; default: "
-                     "num_workers)")
 flags.DEFINE_integer("hidden_units", 100,
                      "Number of units in the hidden layer of the NN")
-flags.DEFINE_integer("train_steps", 100,
+flags.DEFINE_integer("train_steps", 300,
                      "Number of (global) training steps to perform")
 flags.DEFINE_integer("batch_size", 100, "Training batch size")
 flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
@@ -79,7 +78,7 @@ flags.DEFINE_boolean(
     "server.")
 flags.DEFINE_string("ps_hosts", "localhost:2222",
                     "Comma-separated list of hostname:port pairs")
-flags.DEFINE_string("worker_hosts", "localhost:2223,localhost:2224",
+flags.DEFINE_string("worker_hosts", cpustring,
                     "Comma-separated list of hostname:port pairs")
 flags.DEFINE_string("job_name", None, "job name: worker or ps")
 
@@ -95,6 +94,8 @@ IMAGE_PIXELS = 28
 #        'task': {'type': 'worker', 'index': 1}})
 
 def main(unused_argv):
+
+  time_start = time.time()
   # Parse environment variable TF_CONFIG to get job_name and task_index
 
   # If not explicitly specified in the constructor and the TF_CONFIG
@@ -108,8 +109,6 @@ def main(unused_argv):
   FLAGS.task_index = task_index
 
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-  if FLAGS.download_only:
-    sys.exit(0)
 
   if FLAGS.job_name is None or FLAGS.job_name == "":
     raise ValueError("Must specify an explicit `job_name`")
@@ -190,15 +189,12 @@ def main(unused_argv):
 
     y = tf.nn.softmax(tf.nn.xw_plus_b(hid, sm_w, sm_b))
     cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
-
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
 
     if FLAGS.sync_replicas:
-      if FLAGS.replicas_to_aggregate is None:
-        replicas_to_aggregate = num_workers
-      else:
-        replicas_to_aggregate = FLAGS.replicas_to_aggregate
-
+      replicas_to_aggregate = num_workers
       opt = tf.train.SyncReplicasOptimizer(
           opt,
           replicas_to_aggregate=replicas_to_aggregate,
@@ -261,41 +257,54 @@ def main(unused_argv):
       sess = sv.prepare_or_wait_for_session(server.target, config=sess_config)
 
     print("Worker %d: Session initialization complete." % FLAGS.task_index)
-
-    if FLAGS.sync_replicas and is_chief:
-      # Chief worker will start the chief queue runner and call the init op.
-      sess.run(sync_init_op)
-      sv.start_queue_runners(sess, [chief_queue_runner])
-
+       
+    
     # Perform training
     time_begin = time.time()
     print("Training begins @ %f" % time_begin)
-
+    print("Nodes="+str(nodes))
     local_step = 0
+    #control to print data one time every 1000 steps
+    hasPrinted = False
     while True:
       # Training feed
       batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
       train_feed = {x: batch_xs, y_: batch_ys}
 
       _, step = sess.run([train_step, global_step], feed_dict=train_feed)
+      # print("Done step "+str(step))
       local_step += 1
-      print("Node "+str(FLAGS.task_index)+" done step: "+str(step))
       now = time.time()
-      #print("%f: Worker %d: training step %d done (global step: %d)" %(now, FLAGS.task_index, local_step, step))
-
+      if (step % 1000 <= 200 and not hasPrinted and step >= 1000):
+        print("time: %f, step: %d" % (now, step-(step%1000)))
+        hasPrinted = True
+      if (step % 1000 > 200):
+        hasPrinted = False
       if step >= FLAGS.train_steps:
         break
 
     time_end = time.time()
     print("Training ends @ %f" % time_end)
+    print("Steps: %d, Batch size: %d" % (FLAGS.train_steps, FLAGS.batch_size))
     training_time = time_end - time_begin
+    starting_time = time_begin - time_start
+    print("Starting time (time lost before starting training): %f s" % starting_time)
     print("Training elapsed time: %f s" % training_time)
+    
+    if is_chief:
+      # Validation feed
+      val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
+      val_acc = sess.run(accuracy, feed_dict=val_feed)
+      print("Validation accuracy = %g" %
+            (val_acc*100))
+  
+  
+      # Test feed
+      test_feed = {x: mnist.test.images, y_: mnist.test.labels}
+      test_acc = sess.run(accuracy, feed_dict=test_feed)
+      print("Test accuracy = %g" %
+            (test_acc*100))
 
-    # Validation feed
-    val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
-    val_xent = sess.run(cross_entropy, feed_dict=val_feed)
-    print("After %d training step(s), validation cross entropy = %g" %
-          (FLAGS.train_steps, val_xent))
 
 
 if __name__ == "__main__":
