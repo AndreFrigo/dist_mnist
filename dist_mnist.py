153a959279b727317f6940cce32efb53192be9c5
AndreFrigo <45069367+AndreFrigo@users.noreply.github.com>
2023-11-29 16:42:12 +0100

    Update dist_mnist.py

--- dist_mnist.py
+++ dist_mnist.py
@@ -27,7 +27,7 @@ flags.DEFINE_integer("task_index", None,
                      "initialization ")
 flags.DEFINE_integer("num_gpus", 0, "Total number of gpus for each machine."
                      "If you don't use GPU, please set it to '0'")
-flags.DEFINE_integer("train_steps", 50,
+flags.DEFINE_integer("train_steps", 2000,
                      "Number of (global) training steps to perform")
 flags.DEFINE_integer("batch_size", 100, "Training batch size")
 flags.DEFINE_float("learning_rate", 10e-4, "Learning rate")
@@ -225,8 +225,8 @@ with tf.device(tf.train.replica_device_setter(
         batch = mnist.train.next_batch(FLAGS.batch_size)
         _, step = sess.run([train_step, global_step], feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
         now = time.time()
-        if step%10==0:
-            print("time: %f, step: %d" % (now, step-(step%10)))
+        if step%100==0:
+            print("time: %f, step: %d" % (now, step-(step%100)))
         if step >= FLAGS.train_steps: break
         
 
@@ -250,8 +250,10 @@ with tf.device(tf.train.replica_device_setter(
         # test_feed = {x: mnist.test.images, y_: mnist.test.labels}
         # test_acc = sess.run(accuracy, feed_dict=test_feed)
         # print("Test accuracy = %g" % (test_acc*100))
-        test_acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
-        print("Test accuracy = %g" % (test_acc * 100))
+        pass
+        
+    test_acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+    print("Test accuracy = %g" % (test_acc * 100))
 
 
     # with tf.Session() as sess:
