37b28d61c3c4a17aad0879ccaef41d91227294aa
AndreFrigo <45069367+AndreFrigo@users.noreply.github.com>
2023-12-02 10:34:38 +0100

    prova con is chief

--- dist_mnist.py
+++ dist_mnist.py
@@ -257,17 +257,18 @@ def main(unused_argv):
     starting_time = time_begin - time_start
     print("Starting time (time lost before starting training): %f s" % starting_time)
     print("Training elapsed time: %f s" % training_time)
-    
-    # Validation feed
-    val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
-    val_acc = sess.run(accuracy, feed_dict=val_feed)
-    print("Validation accuracy = %g" % (val_acc*100))
-    
-    
-    # Test feed
-    test_feed = {x: mnist.test.images, y_: mnist.test.labels}
-    test_acc = sess.run(accuracy, feed_dict=test_feed)
-    print("Test accuracy = %g" % (test_acc*100))
+
+    if is_chief:
+      # Validation feed
+      val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
+      val_acc = sess.run(accuracy, feed_dict=val_feed)
+      print("Validation accuracy = %g" % (val_acc*100))
+      
+      
+      # Test feed
+      test_feed = {x: mnist.test.images, y_: mnist.test.labels}
+      test_acc = sess.run(accuracy, feed_dict=test_feed)
+      print("Test accuracy = %g" % (test_acc*100))
 
 
 
